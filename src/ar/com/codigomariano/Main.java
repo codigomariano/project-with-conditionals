84f17117e5912345b5c86eab322dbc7da0db7f18
Mariano Sciuto <marianosciuto.ok@gmail.com>
2025-06-13 17:10:13 -0300

    Commit inicial para subir los archivos al proyecto

---
+++ src/ar/com/codigomariano/Main.java
@@ -0,0 +1,21 @@
+package ar.com.codigomariano;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int precio = 100;
+		boolean vencido = false;
+		String nombre;
+		
+		if(precio < 200) {
+			System.out.println("El producto es demasiado económico");
+		} else if (precio >=200 && precio <500 && !vencido) {
+			System.out.println("El producto es el adecuado para comprar");
+		} else if (precio >= 500 || vencido) {
+			System.out.println("No vale la pena comprar este producto");
+		} else {
+			System.out.println("No se puede determinar si conviene comprar este producto");
+		}
+	}
+
+}
